ab144c725544ee5df4b956f2515999b545d8fae8
1510537 <1510537@rgu.ac.uk>
2016-02-24 16:29:31 +0000

    testing

--- index.php
+++ index.php
@@ -13,17 +13,31 @@
     if ($myage >= 21) {
         echo "buy sausage rolls, specs and mugs";
     }
-    elseif  ($myage >= 16) {
+    elseif  ($myage >= 18) {
         echo "buy mugs and specs";
     }
-    elseif ($myage >= 18) {
+    elseif ($myage >= 16) {
         echo "buy only specs";
     }
     else {echo "nothing";
     }
     ?>
+    <p>
     <?php
-
+    $wantedgood = 2;
+    switch ($wantedgood) {
+        case 1:
+           echo "if you're 21 you can buy sausage rolls, specs and mugs";
+            break;
+        case 2:
+            echo "if you're 18 you can buy mugs and specs";
+            break;
+        case 3:
+            echo "if you're 16 you can buy only specs";
+            break;
+        default:
+            echo "you can buy nothing";
+    }
     ?>
 </p>
 </body>
