305883b0855c1755e8f92e8a074c2b455ca8ca5f
iliyaganama <1510537@rgu.ac.uk>
2016-02-27 20:27:01 +0000

    test

--- index.php
+++ index.php
@@ -8,5 +8,12 @@
     echo 5 * 7;
     ?>
 </p>
+<p>
+    <?php
+    $myname ="Frodo Baggins";
+    $myage= 111;
+    echo "My name is" . $myname . "and i am" . $myage;
+    ?>
+</p>
 </body>
 </html>
