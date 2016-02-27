b3ba20ceb91f2f63af53f820f33f523f2b628bd4
iliyaganama <1510537@rgu.ac.uk>
2016-02-27 20:20:30 +0000

    test

--- index.php
+++ index.php
@@ -7,38 +7,5 @@
     echo "Hello World";
     ?>
 </p>
-<p>
-    <?php
-    $myage = "20";
-    if ($myage >= 21) {
-        echo "buy sausage rolls, specs and mugs";
-    }
-    elseif  ($myage >= 18) {
-        echo "buy mugs and specs";
-    }
-    elseif ($myage >= 16) {
-        echo "buy only specs";
-    }
-    else {echo "nothing";
-    }
-    ?>
-    <p>
-    <?php
-    $wantedgood = "specs";
-    switch ($wantedgood) {
-        case "all":
-           echo "if you're 21 you can buy sausage rolls, specs and mugs";
-            break;
-        case "mugsandspecs":
-            echo "if you're 18 you can buy mugs and specs";
-            break;
-        case "specs":
-            echo "if you're 16 you can buy only specs";
-            break;
-        default:
-            echo "you can buy nothing";
-    }
-    ?>
-</p>
 </body>
 </html>
