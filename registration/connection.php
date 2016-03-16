81fb1b3783ca0c5adbef7d8d5219e471d7ba9cd7
1510537 <1510537@rgu.ac.uk>
2016-03-16 16:30:33 +0000

    adminer

---
+++ registration/connection.php
@@ -0,0 +1,8 @@
+<?php
+define('DB_SERVER', 'eu-cdbr-azure-west-d.cloudapp.net');
+define('DB_USERNAME', 'b147602f41b7c0');
+define('DB_PASSWORD', '21349eb1');
+define('DB_DATABASE', 'Ganama');
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+?>
