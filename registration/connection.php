a130b930d04cd71cc83d0d78364f946366add1df
iliyaganama <1510537@rgu.ac.uk>
2016-04-12 23:19:09 +0100

    adminer

--- registration/connection.php
+++ registration/connection.php
@@ -1,8 +1,14 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: iliyaganama
+ * Date: 12/04/2016
+ * Time: 11:17 PM
+ */
 define('DB_SERVER', 'eu-cdbr-azure-west-d.cloudapp.net');
-define('DB_USERNAME', 'b147602f41b7c0');
-define('DB_PASSWORD', '21349eb1');
-define('DB_DATABASE', 'Ganama');
+define('DB_USERNAME', 'b896bf3ed7b5f5');
+define('DB_PASSWORD', '25d1fd49');
+define('DB_DATABASE', 'NetSecCW');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
 ?>
