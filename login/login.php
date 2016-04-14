35eff66d1d9c71c3127a790c68df6c26f57787fa
iliyaganama <1510537@rgu.ac.uk>
2016-04-15 00:18:35 +0100

    change

--- login/login.php
+++ login/login.php
@@ -5,7 +5,7 @@
 	include("connection.php"); //Establishing connection with our database
 	
 	$error = ""; //Variable for storing our errors.
-	if(isset($_POST["submit"]))
+	if(isset($_GET["submit"]))
 	{
 
 			// Define $username and $password
