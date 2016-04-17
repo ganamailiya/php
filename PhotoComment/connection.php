bb5798ebfa088618225d98da04c984947ebc832c
iliyaganama <1510537@rgu.ac.uk>
2016-04-17 01:13:12 +0100

    photo

--- PhotoComment/connection.php
+++ PhotoComment/connection.php
@@ -1,8 +1,8 @@
 <?php
-$user = 'root';
-$pass = '';
-$db = 'ganama';
-
-$db = new mysqli('localhost', $user, $pass, $db) or die("unable to connect");
+define('DB_SERVER', 'eu-cdbr-azure-west-d.cloudapp.net');
+define('DB_USERNAME', 'b2b2eb5a9bed89');
+define('DB_PASSWORD', '965d05ef');
+define('DB_DATABASE', 'Uni1510537');
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
 ?>
